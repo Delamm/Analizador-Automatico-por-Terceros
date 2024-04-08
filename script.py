62eae560b8255a3d71b40ccf402a4fd446fa5b0c
Delamm <eldygonzalez.09@hotmail.com>
2024-04-08 00:49:39 -0300

    Manejo de errores

--- script.py
+++ script.py
@@ -3,10 +3,12 @@ import pyperclip # Sirve para copiar y pegar cosas en python
 import pyautogui # Automatizar que se ejecute una tecla por ej el espacio
 import time # Nos va a ayudar a evitar errores
 from colorama import Fore, Style # Colores
-import threading # Optimizando la herramienta
 
-with open('addresses.txt','r') as f:
-    f = f.read().split('\n')
+try:
+    with open('addresses.txt','r') as f:
+        f = f.read().split('\n')
+except FileNotFoundError:
+    raise FileNotFoundError("No se ha encontrado el archivo con las direcciones IP")
 
 election = input(Fore.GREEN + "Escribe el nombre de la herramienta que quieras utilizar para analizar las direcciones IP: "
 + Fore.YELLOW + """\n1 - Symantec
@@ -15,25 +17,28 @@ election = input(Fore.GREEN + "Escribe el nombre de la herramienta que quieras u
 
 election = int(election)
 
-def open_browser(url,tab=0):
-    for ip in f:
-        webbrowser.open_new(url)
-        time.sleep(3)
-        for i in range(tab):
-            pyautogui.press('tab')
-        pyperclip.copy(ip)
-        pyautogui.hotkey('ctrl','v', interval=0.15)
-        pyautogui.press('enter')
+def open_browser(url,tab=0):    
+    webbrowser.open_new(url)
+    time.sleep(3)
+    for i in range(tab):
+        pyautogui.press('tab')
+    pyperclip.copy(ip)
+    pyautogui.hotkey('ctrl','v', interval=0.15)
+    pyautogui.press('enter')
 
  # El usuario elige Symantec
 if election == 1:
-    open_browser("https://sitereview.bluecoat.com/#/")
+    for ip in f:
+        open_browser("https://sitereview.bluecoat.com/#/")
  # El usuario elige AbuseIP
 elif election == 2:
-    open_browser("https://www.abuseipdb.com/",15)    
+    for ip in f:
+        open_browser("https://www.abuseipdb.com/",15)
+           
  # El usuario elige VirusTotal
 elif election == 3:
-    open_browser("https://www.virustotal.com/gui/home/search",6)
+    for ip in f:
+        open_browser("https://www.virustotal.com/gui/home/search",)
     
 else:
     raise ValueError("Debe insertar numeros del 1 al 3 inclusive.")
