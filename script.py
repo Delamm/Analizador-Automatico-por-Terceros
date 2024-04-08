c5ed8f61182c6cd9987d7a2048cb9b2d4b9ba588
Delamm <eldygonzalez.09@hotmail.com>
2024-04-08 00:23:33 -0300

    Modularizando

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import pyperclip # Sirve para copiar y pegar cosas en python
 import pyautogui # Automatizar que se ejecute una tecla por ej el espacio
 import time # Nos va a ayudar a evitar errores
 from colorama import Fore, Style # Colores
+import threading # Optimizando la herramienta
 
 with open('addresses.txt','r') as f:
     f = f.read().split('\n')
@@ -13,34 +14,26 @@ election = input(Fore.GREEN + "Escribe el nombre de la herramienta que quieras u
 3 - VirusTotal: \n""" + Style.RESET_ALL + "Su elección --> ")
 
 election = int(election)
+
+def open_browser(url,tab=0):
+    for ip in f:
+        webbrowser.open_new(url)
+        time.sleep(3)
+        for i in range(tab):
+            pyautogui.press('tab')
+        pyperclip.copy(ip)
+        pyautogui.hotkey('ctrl','v', interval=0.15)
+        pyautogui.press('enter')
+
  # El usuario elige Symantec
 if election == 1:
-    for ip in f:
-        webbrowser.open_new("https://sitereview.bluecoat.com/#/")
-        time.sleep(3)
-        pyperclip.copy(ip)
-        pyautogui.hotkey('ctrl','v', interval=0.05)
-        pyautogui.press('enter')
+    open_browser("https://sitereview.bluecoat.com/#/")
  # El usuario elige AbuseIP
 elif election == 2:
-    for ip in f:
-        webbrowser.open_new("https://www.abuseipdb.com/")
-        time.sleep(2.5)
-        # Para poder pegar algo primero tenemos que llegar al input apretando 15 veces TAB
-        for i in range(15):
-            pyautogui.press('tab')
-        pyperclip.copy(ip)
-        pyautogui.hotkey('ctrl','v', interval=0.15)
-        pyautogui.press('enter')
+    open_browser("https://www.abuseipdb.com/",15)    
  # El usuario elige VirusTotal
 elif election == 3:
-    for ip in f:
-        webbrowser.open_new("https://www.virustotal.com/gui/home/search")
-        time.sleep(2.5)
-        for i in range(6):
-            pyautogui.press('tab')
-        pyperclip.copy(ip)
-        pyautogui.hotkey('ctrl','v', interval=0.15)
-        pyautogui.press('enter')
+    open_browser("https://www.virustotal.com/gui/home/search",6)
+    
 else:
     raise ValueError("Debe insertar numeros del 1 al 3 inclusive.")
